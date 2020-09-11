0e4a0736fc21d722f2e7201b1b8a123f76961f34
q1q0374 <55523902+q1q0374@users.noreply.github.com>
2020-09-11 23:32:28 +0900

    Add files via upload

---
+++ week-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("201687045 박진홍");
+        }
+    }
+}
