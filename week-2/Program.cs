5979724af1e0840fee8368dd66b72266c72b74b8
q1q0374 <55523902+q1q0374@users.noreply.github.com>
2020-09-19 15:08:15 +0900

    Add files via upload

---
+++ week-2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace re2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 3;
+
+
+            Console.WriteLine("a="+a+"  b="+3);
+            Console.WriteLine("a+b=" + (a + b));
+            Console.WriteLine("a-b=" + (float)(b - a));
+            Console.WriteLine("a*b=" + (a * b));
+            Console.WriteLine("a/b=" + (double)a/b);
+
+
+        }
+    }
+}
