b8269db53f9491d93ab8fc330e1de9e88707234e
q1q0374 <55523902+q1q0374@users.noreply.github.com>
2020-09-24 17:05:22 +0900

    Add files via upload

---
+++ week-3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            string ipt;
+            Console.WriteLine("sex");
+            Console.WriteLine("정수 입력 : ");
+            abc:
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("결과");
+            Console.WriteLine($"더하기 : {num1+num2}");
+            Console.WriteLine($"뺴기 : { num1 - num2}");
+            Console.WriteLine(num1 * num2);
+            Console.WriteLine((double)num1 / num2);
+            goto abc;
+
+        }
+    }
+}
