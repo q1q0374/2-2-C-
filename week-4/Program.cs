17912c8f6db36badecd8bc715b4951a55ddf0120
q1q0374 <55523902+q1q0374@users.noreply.github.com>
2020-10-12 23:14:58 +0900

    Add files via upload

---
+++ week-4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace callbyvalue
+{
+    class Program
+    {
+
+        static void Swap(int x, int y)
+        {
+            int temp;
+            temp = x; x = y; y = temp;
+            Console.WriteLine("Swap x= {0}, y= {1}", x, y);
+        }
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            Console.WriteLine("before x= {0}, y= {1}",x,y);
+            Swap(x, y);
+            Console.WriteLine("after x= {0}, y= {1}", x, y);
+        }
+    }
+}
