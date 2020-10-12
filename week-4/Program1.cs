a77f76ff0377d539732978581461ccc00fed05e0
q1q0374 <55523902+q1q0374@users.noreply.github.com>
2020-10-12 23:17:03 +0900

    Add files via upload

---
+++ week-4/Program1.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+
+
+namespace z
+{
+    class Program
+    {
+        static void Swap(ref int a, ref int b)
+        {
+            int temp;
+            temp = a; a = b; b = temp;
+            Console.WriteLine("Swap:x={0},y={1}", a, b);
+
+        }
+        static void Main(string[] args)
+        {
+            int x = 1, y = 2;
+            Console.WriteLine("Before: x={0},y={1}", x, y);
+            Swap(ref x, ref y);
+            Console.WriteLine("after: x={0},y={1}", x, y);
+
+
+        }
+    }
+}
